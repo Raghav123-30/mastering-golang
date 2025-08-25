caa1f2cad618d4722d3fd8c8c047862411c2a879
Raghav123-30 <raghavendrabhat2018@gmail.com>
2025-08-25 19:26:10 +0530

    Practice golang variables

--- main.go
+++ main.go
@@ -1,5 +1,40 @@
 package main
 
 func main(){
-  println("Lets get started")
+  //Explicit type declaration
+  var x int
+  x = 10
+  println(x)
+
+  // Shorthand declaration, go infers the type
+  y := 42
+  println(y)
+
+  // multiple declaration
+  var m, n = 3, "hello"
+  println(m,n)
+
+  //shorthand multiple declaration
+  a, b := 3,4
+  println(a,b)
+
+  x = 5
+  if true{
+    x := 10 //shadows outer x
+    println(x)
+  }
+  println(x)
+
+  //constants
+  const Pi = 3.14
+
+  //constants are untyped ,allows assignment to int32 variable
+  const Max = 1000
+  var z int32 = Max
+
+  //pointer
+  g := 10;
+  p := &g;
+
+  println(z,g,p)
 }
